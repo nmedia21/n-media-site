dc034f10bbeef062e39c545cf29687597425b75d
nmedia21 <nenil67@hotmail.fr>
2026-02-11 13:25:51 +0100

    Fix next config pour GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Cela permet de déployer même s'il y a des erreurs d'apostrophes
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // On ignore aussi les erreurs TypeScript pour être sûr que ça passe
-    ignoreBuildErrors: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
 };
 
